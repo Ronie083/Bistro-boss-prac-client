047829b8c575fc9601731bea8e8bdf7bdf9005b3
Ronie083 <ahmedronie6@gmail.com>
2023-06-01 20:34:55 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='font-bold text-red-600'>Project getting ready</h1>
+        </div>
+    );
+};
+
+export default Home;
